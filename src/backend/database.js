433551a66aa4f82bb0a3de39adc47c006ec8d156
bandilem-git <u24675394@tuks.co.za>
2025-09-13 14:26:41 +0200

    Able to insert User

--- src/backend/database.js
+++ src/backend/database.js
@@ -5,24 +5,53 @@ const password = process.env.DB_PASSWORD;
 const uri = `mongodb+srv://${username}:${password}@intervarsityhackathon20.yqnn4mm.mongodb.net/?retryWrites=true&w=majority&appName=IntervarsityHackathon2025ConcreteBoysCluster`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi:{
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors:true,
-    }
-});
+// const client = new MongoClient(uri, {
+//     serverApi:{
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors:true,
+//     }
+// });
 
-async function run() {
-    try {
-        // Connect client to the server 
+// async function run() {
+//     try {
+//         // Connect client to the server 
+//         await client.connect();
+//         // Send ping to confirm a successful connection
+//         await client.db("admin").command({ping:1});
+//         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//     }finally{
+//         await client.close();
+//     }
+// }
+
+// run().catch(console.dir);
+let client;
+let db;
+// connect to the database
+async function connectDB() {
+    if (!client) {
+        client = new MongoClient(uri, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            }
+        });
         await client.connect();
-        // Send ping to confirm a successful connection
-        await client.db("admin").command({ping:1});
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    }finally{
+        db = client.db('IntervarsityHackathon');
+    } else if (!db) {
+        db = client.db('IntervarsityHackathon');
+    }
+    return db;
+}
+//close connection
+async function closeDB() {
+    if (client) {
         await client.close();
+        client = null;
+        db = null;
     }
 }
 
-run().catch(console.dir);
+module.exports = { connectDB, closeDB };
