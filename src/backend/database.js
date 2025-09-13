c7cb45dffd6fac2d913031f3ce733bedc8a639ba
bandilem-git <u24675394@tuks.co.za>
2025-09-13 14:47:39 +0200

    reflected! should insert a user

--- src/backend/database.js
+++ src/backend/database.js
@@ -4,25 +4,55 @@ const username = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 const uri = `mongodb+srv://${username}:${password}@intervarsityhackathon20.yqnn4mm.mongodb.net/?retryWrites=true&w=majority&appName=IntervarsityHackathon2025ConcreteBoysCluster`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi:{
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors:true,
-    }
-});
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//     serverApi:{
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors:true,
+//     }
+// });
 
-async function run() {
-    try {
-        // Connect client to the server 
+// async function run() {
+//     try {
+//         // Connect client to the server 
+//         await client.connect();
+//         // Send ping to confirm a successful connection
+//         await client.db("admin").command({ping:1});
+//         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//     }finally{
+//         await client.close();
+//     }
+// }
+
+// run().catch(console.dir);
+
+
+let client;
+
+// Connect to DB
+async function connectDB() {
+    if (!client) {
+        client = new MongoClient(uri, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            },
+        });
         await client.connect();
-        // Send ping to confirm a successful connection
-        await client.db("admin").command({ping:1});
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    }finally{
+        console.log('Connected to MongoDB');
+    }
+    return client.db('IntervarsityHackathon');
+}
+
+// Close DB connection
+async function closeDB() {
+    if (client) {
         await client.close();
+        console.log('MongoDB connection closed');
+        client = null;
     }
 }
 
-run().catch(console.dir);
+module.exports = { connectDB, closeDB };
